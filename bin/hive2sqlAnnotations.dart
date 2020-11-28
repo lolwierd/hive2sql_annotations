0a050b628b1c37691bf9d26ab37a7859754263ae
Ayaan Retiwala <ayaan.180410107098@gmail.com>
2020-11-28 21:32:49 +0530

    Initial Commit

---
+++ bin/hive2sqlAnnotations.dart
@@ -0,0 +1,5 @@
+import 'package:hive2sqlAnnotations/hive2sqlAnnotations.dart' as hive2sqlAnnotations;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hive2sqlAnnotations.calculate()}!');
+}
---
+++ test/hive2sqlAnnotations_test.dart
@@ -0,0 +1,8 @@
+import 'package:hive2sqlAnnotations/hive2sqlAnnotations.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
